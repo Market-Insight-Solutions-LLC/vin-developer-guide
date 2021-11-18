3794d66b22ff0f89842368b814fb6c2471c0fdcb
Dion <dhicks@virgilsystems.com>
2021-11-18 16:25:13 -0330

    testing custom.css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,5 @@
 # Configuration file for the Sphinx documentation builder.
 
-import os
-import sys
-sys.path.append(os.path.abspath('../..'))
-sys.path.append(os.path.abspath('..'))
-
 # -- Project information
 
 project = 'VIN Developer Guide'
