66c5bd91edca4d6d5b4d7f45dd18402d2d1e6d4e
Dion <dhicks@virgilsystems.com>
2022-04-19 12:56:09 -0230

    Testing .ymal file changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,8 +33,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    #'sphinx.ext.sphinx_panels',
-    'sphinx_panels'
+    'sphinx.ext.sphinx_panels',
+    #'sphinx_panels'
 ]
 
 panels_add_bootstrap_css = False
